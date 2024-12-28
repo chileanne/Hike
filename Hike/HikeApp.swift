6e8fa6cd97dfd51ed5333c8b531984576cb0dbfe
ekpei <anthony4arsenal@gmail.com>
2024-12-28 11:23:30 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by user on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
